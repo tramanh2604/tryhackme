303f5fb72075f484a429aa664b41e25696c45ac0
tramanh2604 <buihuynhtramanh911@gmail.com>
2025-02-12 16:02:21 +0700

    hoan thanh window2, 2 tool cho python

---
+++ learning-notes/python/tools/dirnum.py
@@ -0,0 +1,18 @@
+﻿# Hoạt động tương tự như subdomainEnum.py, dựa trên vòng lặp for và bỏ qua những status_code==404
+
+import requests
+import sys
+
+# Đọc danh sách directory tìm năng từ wordlist2.txt
+sub_list = open("D:/tryhackme/learning-notes/python/wordlist2.txt").read()
+directories = sub_list.splitlines()
+
+# Tạo vòng lặp duyệt qua từng directory
+for dir in directories:
+    dir_enum = f"http://{sys.argv[1]}/{dir}.html" #duyệt qua các file html
+    r = requests.get(dir_enum)
+
+    if r.status_code == 404:
+        pass #bỏ qua lỗi
+    else:
+        print("valid directory: ,dir_enum");
---
+++ learning-notes/python/tools/subdomainEnum.py
@@ -0,0 +1,27 @@
+﻿# Chương trình sẽ đọc subdomain từ file wordlist2.txt và kiểm tra xem chúng có phản hồi không
+# Giả định rằng những subdomain chấp nhận kết nối là những subdomain tồn tại
+
+import requests #gửi request, xử lý respone
+import sys
+
+# Đọc danh sách subdomains từ file
+sub_list = open("D:/tryhackme/learning-notes/python/wordlist2.txt").read()
+sub_dom = sub_list.splitlines() #đọc toàn bộ file và tách thành danh sách các dòng
+
+# kiểm tra từng subdomain
+for sub in sub_dom:
+    sub = sub.strip() # xóa khoảng trắng giữa
+    sub_domain = f"http://{sub}.{sys.argv[1]}"
+
+    #sys.argv[1]: đối số đầu tiên đc truyền vào
+    # khi chạy lệnh `python3 SubdomainEnumeration.py example.com` thì chương trình sẽ
+    # tạo ra các URL dạng http:// blog.example.com
+
+    try:
+        requests.get(sub_domain)
+
+    except requests.ConnectionError:
+        pass # bỏ qua nếu k thể kết nối
+
+    else:
+        print("Valid domain: ", sub_domain) # in domain hợp lệ
